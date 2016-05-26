6096eca754bda5e447234a65da08c2322533f519
David Perkins <perkinsdavid14@gmail.com>
2016-05-25 23:07:06 -0700

    init commit

---
+++ SimpleWeather/SimpleWeather/WXController.h
@@ -0,0 +1,14 @@
+//
+//  WXController.h
+//  SimpleWeather
+//
+//  Created by David Perkins on 5/25/16.
+//  Copyright © 2016 David Perkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXController : UIViewController <UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate>
+
+
+@end
